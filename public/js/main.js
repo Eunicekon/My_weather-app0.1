d884e5c9c8f500699fc9dd32af51179fcc8b9f61
Eunice K <kontor.eunice@gmail.com>
2020-01-01 16:43:35 +0000

    modal added

--- public/js/main.js
+++ public/js/main.js
@@ -51,3 +51,34 @@ function fetchWeather(city) {
 }
 
 buildDropdown();
+
+//get modal element
+var modal = document.getElementById('simpleModal');
+//get open modal button
+var modalBtn = document.getElementById('modalBtn');
+//get close button
+var closeBtn = document.getElementsByClassName('closeBtn');
+
+//Listen for open click
+modalBtn.addEventListener('click', openModal);
+//Listen for close click
+closeBtn.addEventListener('click', closeModal);
+//Listen for outside click
+window.addEventListener('click', outsideClick);
+
+//function to open modal
+function openModal(){
+    modal.style.display = 'block';
+}
+
+//function to close modal
+function closeModal(){
+    modal.style.display = 'none';
+}
+
+//function to close modal if outside click
+function outsideClick(e){
+    if(e.target == modal){
+    modal.style.display = 'none';
+    }
+}
