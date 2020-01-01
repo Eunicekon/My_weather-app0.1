8089b6a2065a725fa1f2808655baa207655b07ec
Eunice K <kontor.eunice@gmail.com>
2020-01-01 16:36:58 +0000

    modal

--- public/js/main.js
+++ public/js/main.js
@@ -5,37 +5,7 @@ const cities = [{name: 'Dubai', value: 'Dubai'}, {name: 'London', value: 'London
 {name: 'New York', value: 'New York'}, {name: 'Accra', value: 'Accra'}, {name: 'Cape Town', value: 'Cape Town'}];
 
 
-//get modal element
-var modal = document.getElementById('simpleModal');
-//get open modal button
-var modalBtn = document.getElementById('modalBtn');
-//get close button
-var closeBtn = document.getElementsByClassName('closeBtn');
 
-//Listen for open click
-modalBtn.addEventListener('click', openModal);
-//Listen for close click
-closeBtn.addEventListener('click', closeModal);
-//Listen for outside click
-window.addEventListener('click', outsideClick);
-
-//function to open modal
-function openModal(){
-    modal.style.display = 'block';
-}
-
-//function to close modal
-function closeModal(){
-    modal.style.display = 'none';
-}
-
-//function to close modal if outside click
-function outsideClick(e){
-    if(e.target == modal){
-    modal.style.display = 'none';
-    }
-}
-    
 
 function buildDropdown(){
     const selectElement = document.getElementById("select");
